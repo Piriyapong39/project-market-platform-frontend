4e3f3635462142ff06dfa6e2388f60d2584400e7
Azujito <virote3011@gmail.com>
2024-10-29 11:45:58 +0700

    I do not know

--- src/pages/seller/seller-homepage/homepage-seller.js
+++ src/pages/seller/seller-homepage/homepage-seller.js
@@ -3,7 +3,7 @@ const token = localStorage.getItem("token")
 
 document.addEventListener("DOMContentLoaded", async ()=>{
     if(!token){
-        // window.location.href = "../../login/login.html"
+        window.location.href = "../../login/login.html"
     }
     const response = await fetch("http://localhost:8080/users/sellers/authen", {
         method: "POST",
